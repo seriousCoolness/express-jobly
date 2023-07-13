3eb232cb8d6650f383e6849eb090c66dd277baad
robert <wyattrobert42@gmail.com>
2023-07-13 03:35:59 -0400

    progress on first task

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,11 +1,14 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
-
+/** Takes json data and a js-to-sql name dictionary, 
+ * and returns the data as sql column names and values.
+ * Use this to make table updating less of a hassle.
+ * Returns { setCols, values } */
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
+  // Maps the json data to sql column names and parameter values using the jsToSql dict.
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
---
+++ helpers/sql.test.js
@@ -0,0 +1,7 @@
+const { sqlForPartialUpdate } = require("./sql");
+
+describe("prepareDataForUpdate", function () {
+    test("works: column-name translation", function () {
+        
+    })
+});
