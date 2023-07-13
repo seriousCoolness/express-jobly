42645f29fc8d659a30fce7202c9c2842b40767db
robert <wyattrobert42@gmail.com>
2023-07-13 03:49:33 -0400

    created sql helper test

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -2,6 +2,11 @@ const { sqlForPartialUpdate } = require("./sql");
 
 describe("prepareDataForUpdate", function () {
     test("works: column-name translation", function () {
+        const testData = {firstName: 'Aliya', age: 32};
+        const testJsToSql = {firstName: "first_name", age: "age"};
         
-    })
+        const colAndValues = sqlForPartialUpdate(testData, testJsToSql);
+        expect(colAndValues.setCols).toEqual(`"first_name"=$1, "age"=$2`);
+        expect(colAndValues.values).toEqual(['Aliya', 32]);
+    });
 });
